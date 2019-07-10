35a6af01956d092f960890d369746c30689111dd
ligrudler <grudler@MacBook-Pro-de-Grudler.local>
2019-07-10 15:48:08 +0200

    norme Julia.c

--- julia.c
+++ julia.c
@@ -6,7 +6,7 @@
 /*   By: grudler <grudler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/09 23:23:06 by grudler           #+#    #+#             */
-/*   Updated: 2019/07/09 23:53:57 by grudler          ###   ########.fr       */
+/*   Updated: 2019/07/10 15:38:34 by grudler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,31 @@ void	init_var_julia(t_mlx *pmlx)
 	pmlx->x = 0;
 	pmlx->x1 = -1;
 	pmlx->y1 = -1.2;
-	pmlx->it_max = 150;
+	pmlx->it_max = 300;
 	pmlx->color = 265;
+}
+
+void	julia_calc(t_mlx *pmlx)
+{
+	double tmp;
+
+	pmlx->c_r = 0.285;
+	pmlx->c_i = 0.01;
+	pmlx->z_r = pmlx->x / pmlx->zoom + pmlx->x1;
+	pmlx->z_i = pmlx->y / pmlx->zoom + pmlx->y1;
+	pmlx->it = 0;
+	while (pmlx->z_r * pmlx->z_r + pmlx->z_i * pmlx->z_i < 4 && pmlx->it < pmlx->it_max)
+	{
+		tmp = pmlx->z_r;
+		pmlx->z_r = pmlx->z_r * pmlx->z_r - pmlx->z_i * pmlx->z_i + pmlx->c_r;
+		pmlx->z_i = 2 * pmlx->z_i * tmp + pmlx->c_i;
+		pmlx->it++;
+	}
 }
 
 int		julia(void *param)
 {
 	t_mlx		*pmlx;
-	double		tmp;
 
 	pmlx = (t_mlx *)param;
 	init_var_julia(pmlx);
@@ -35,18 +52,7 @@ int		julia(void *param)
 		pmlx->y = 0;
 		while (pmlx->y < WINY)
 		{
-			pmlx->c_r = 0.285;
-			pmlx->c_i = 0.01;
-			pmlx->z_r = pmlx->x / pmlx->zoom + pmlx->x1;
-			pmlx->z_i = pmlx->y / pmlx->zoom + pmlx->y1;
-			pmlx->it = 0;
-			while (pmlx->z_r * pmlx->z_r + pmlx->z_i * pmlx->z_i < 4 && pmlx->it < pmlx->it_max)
-			{
-				tmp = pmlx->z_r;
-				pmlx->z_r = pmlx->z_r * pmlx->z_r - pmlx->z_i * pmlx->z_i + pmlx->c_r;
-				pmlx->z_i = 2 * pmlx->z_i * tmp + pmlx->c_i;
-				pmlx->it++;
-			}
+			julia_calc(pmlx);
 			if (pmlx->it != pmlx->it_max)
 				put_pixel(pmlx);
 			pmlx->y++;
